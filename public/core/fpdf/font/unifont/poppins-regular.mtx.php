5e4dd85995e55c9e8957f83b76080cc737340708
mariusz-pacyga-wttech <mariusz.pacyga@wundermanthompson.com>
2023-03-15 16:24:04 +0100

    first mock implementation

---
+++ public/core/fpdf/font/unifont/poppins-regular.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='Poppins-Regular';
+$type='TTF';
+$desc=array (
+  'Ascent' => 1050.0,
+  'Descent' => -350.0,
+  'CapHeight' => 698.0,
+  'Flags' => 4,
+  'FontBBox' => '[-531 -572 2567 1065]',
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 500.0,
+);
+$up=-75;
+$ut=50;
+$ttffile='/home/krainadzieciaka/websites/wp_1/wp-content/plugins/kraina-summary/core/fpdf/font/unifont/Poppins-Regular.ttf';
+$originalsize=158192;
+$fontkey='poppins';
+?>
---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { flowResult } from 'mobx';
+import Store from './store/store';
+import { Client } from './modules/client/client';
+import { WP_ACTIONS } from './modules/constants';
+import handlers from './modules/stubs/handlers';
+import { setupWorker } from 'msw';
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('react-spa-container') as HTMLElement
+);
+const init = async () => {
+    if(import.meta.env.DEV) {
+        const worker = setupWorker(...handlers);
+        await worker.start().catch(() => console.error('Worker error'));
+    }
+
+    const client = new Client(WP_ACTIONS.AJAX_URL);
+    const store = new Store({ client });
+    await flowResult(store.fetchOrders());
+}
+
+init().then(() => {
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+})
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
